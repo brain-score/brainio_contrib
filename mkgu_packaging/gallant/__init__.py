57ee05971df503aed0b5a88554b33cb34ebf7e5c
Martin Schrimpf <m4rtinsch@gmail.com>
2018-04-25 16:24:03 -0400

    make script flexible to handle V1/2 data; add additional data properties

--- mkgu_packaging/gallant/__init__.py
+++ mkgu_packaging/gallant/__init__.py
@@ -1,37 +1,56 @@
 import glob
 import os
 
+import argparse
 import numpy as np
 import pandas as pd
 import xarray as xr
 
 
 def main():
-    directory = os.path.join(os.path.dirname(__file__), 'V1Data', 'NatRev')
-    data_files = glob.glob(os.path.join(directory, 'data', '*.csv'))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--directory', type=str, default=os.path.join('V1Data', 'NatRev'))
+    args = parser.parse_args()
+    print("Running with args {}".format(vars(args)))
+
+    data_files = glob.glob(os.path.join(args.directory, 'data', '*.csv'))
     data = pd.concat((pd.read_csv(f) for f in data_files))
     num_duplicates = len(data) - len(data.drop_duplicates())
     assert num_duplicates == 0
     data.rename(columns={'cellName': 'neuroid', 'stimuliPaths': 'image_file_name'}, inplace=True)
-    print("Found responses for {} cells, average spike count {:.4f}".format(len(np.unique(data['neuroid'])),
-                                                                            np.mean(data['response'])))
+    neuroids, neuroid_indices = unique_ordered(data['neuroid'].values, return_index=True)
+    print("Found responses for {} cells, average spike count {:.4f}".format(
+        len(neuroids), np.mean(data['response'])))
 
-    responses, neuroids, image_file_names = np.full((len(data), len(np.unique(data['neuroid']))), np.nan), [], []
+    responses = np.full((len(data), len(neuroids)), np.nan)
     row = 0
-    for neuroid_iter, neuroid in enumerate(np.unique(data['neuroid'])):
+    for neuroid_iter, neuroid in enumerate(neuroids):
         neuroid_data = data[data['neuroid'] == neuroid]
         responses[row:row + len(neuroid_data), neuroid_iter] = neuroid_data['response'].values
         row += len(neuroid_data)
-        neuroids.append(neuroid)
-        image_file_names += neuroid_data['image_file_name'].values.tolist()
 
     assembly = xr.DataArray(responses,
-                            coords={'image_file_name': data['image_file_name'], 'neuroid': np.unique(data['neuroid'])},
+                            coords={
+                                'image_file_name': data['image_file_name'],
+                                'category_name': ('image_file_name', data['stimulusCategory']),
+                                'stimulusRepeats': ('image_file_name', data['stimulusRepeats']),
+
+                                'neuroid': neuroids,
+                                'region': ('neuroid', data['area'].iloc[neuroid_indices]),
+                                'animal': ('neuroid', data['animal'].iloc[neuroid_indices])},
                             dims=['image_file_name', 'neuroid'])
     print("Created {} assembly".format(" x ".join(map(str, assembly.shape))))
-    savepath = os.path.abspath(os.path.join(directory, 'data.nc'))
+    savepath = os.path.abspath(os.path.join(args.directory, 'data.nc'))
     assembly.to_netcdf(savepath)
     print("Saved to {}".format(savepath))
+
+
+def unique_ordered(x, return_index=False):
+    _, indices = np.unique(x, return_index=True)
+    ordered_x = x[np.sort(indices)]
+    if not return_index:
+        return ordered_x
+    return ordered_x, indices
 
 
 if __name__ == '__main__':
--- mkgu_packaging/gallant/convertGallant.m
+++ mkgu_packaging/gallant/convertGallant.m
@@ -1,17 +1,22 @@
-function convertGallantV1(dryRun)
+function convertGallant(directory, dryRun)
 if ~exist('dryRun', 'var')
     dryRun = false;
 end
-addpath(genpath('gallant/V1Data/functions'));
-directory = 'gallant/V1Data/NatRev';
-files = glob([directory, '/*/*.mat']);
+if ~exist('directory', 'var')
+    directory = 'V1Data/NatRev';
+end
+addpath(genpath([directory, '/../functions']));
+scriptDir = fileparts(mfilename('fullpath'));
+addpath(genpath([scriptDir, '/lib']));
+files = glob([directory, '/*/*summary_file.mat']);
 for i = 1:length(files)
     file = files{i};
     [subDir, ~, ~] = fileparts(file);
     summaries = load(file);
     summaries = summaries.celldata;
     for summary = summaries'
-        stimuliFile = [subDir, '/', summary.rawstimfile];
+        %% read and align stimuli and responses
+        stimuliFile = [subDir, '/', summary.stimfile];
         stimuli = loadimfile(stimuliFile);
         stimuli = uint8(stimuli);
         responseFile = [subDir, '/', summary.respfile];
@@ -32,20 +37,27 @@ for i = 1:length(files)
         response = response(keptFixation);
         assert(~ismember(-1, response));
 
+        %% write stimuli, create data table
         stimuliPaths = writeStimuli(stimuli, stimuliFile, dryRun);
-        if ~stimuliPaths
-            fprintf('Skipping\n');
-            continue;
-        end
+        [~, stimulusCategory] = fileparts(directory);
+        stimulusCategory = repmat({stimulusCategory}, size(response));
         cellName = repmat({summary.cellid}, size(response));
-        data = table(stimuliPaths, response, cellName);
+        animal = repmat({summary.cellid(1)}, size(response));
+        area = repmat({summary.area}, size(response));
+        stimulusRepeats = repmat({summary.repcount}, size(response));
+        data = table(stimuliPaths, response, stimulusCategory, cellName, area, animal, stimulusRepeats);
+        
+        %% write csv
         [responseFiledir, responseFilename, responseFileext] = fileparts(responseFile);
-        csvPath = [responseFiledir, '/../data/', responseFilename, responseFileext, '.csv'];
+        dataFiledir = [responseFiledir, '/../data/'];
+        csvPath = [dataFiledir, responseFilename, responseFileext, '.csv'];
         if ~dryRun
+            if ~isfolder(dataFiledir)
+                mkdir(dataFiledir);
+            end
             writetable(data, csvPath);
         end
         fprintf('Wrote to %s\n', csvPath);
-        %implay(uint8(stimuli));
     end
 end
 end
@@ -55,9 +67,10 @@ function stimuliPaths = writeStimuli(stimuli, stimuliFile, dryRun)
     [stimuliDir, baseDir, ~] = fileparts(stimuliFile);
     stimuliDir = [stimuliDir, '/stimuli/', baseDir, '/', basename, basenameExt];
     if isfolder(stimuliDir)
-        return false;  % indicate already complete
+        dryRun = true;
+    else
+        mkdir(stimuliDir);
     end
-    mkdir(stimuliDir);
     stimuliPaths = cell(size(stimuli, 3), 1);
     for stimulus = 1:size(stimuli, 3)
         image = stimuli(:, :, stimulus);
