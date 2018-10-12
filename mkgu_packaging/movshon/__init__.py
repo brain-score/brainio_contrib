bfcded0d1c760c2c417db05b3f8fcd7bddf71a84
Jonathan Prescott-Roy <jjpr@mit.edu>
2018-10-12 15:05:18 -0400

    Aligning the assembly presentation axis with correct images.  Still need to write to disk and make StimulusSet lookup.

--- mkgu_packaging/movshon/__init__.py
+++ mkgu_packaging/movshon/__init__.py
@@ -7,53 +7,70 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 
+from brainscore.knownfile import KnownFile as kf
 
-def load_stimuli(data_path):
-    integer_fields = ['family', 'sample']
-    stimuli_directory = os.path.join(data_path, 'stim')
+
+# from FreemanZiemba2013_V1V2data_readme.m
+textureNumOrder = [327, 336, 393, 402, 13, 18, 23, 30, 38, 48, 52, 56, 60, 71, 99]
+
+
+def load_stimuli(stimuli_directory):
     stimuli = []
     for filepath in glob(f"{stimuli_directory}/*.png"):
-        # sample filename: noise-320x320-im13-smp8
         filename = os.path.basename(filepath)
-        pattern = "^(?P<type>[^-]+)-(?P<resolution>[^-]+)-im(?P<family>[0-9]*)-smp(?P<sample>[0-9]+)\.png$"
-        match = re.match(pattern, filename)
-        assert match
-        fields = match.groupdict()
-        fields = {field: value if field not in integer_fields else int(value) for field, value in fields.items()}
+        fields = fields_from_image_name(filename)
         stimuli.append({**fields, **{'filepath': filepath, 'filename': filename}})
 
     stimuli = pd.DataFrame(stimuli)
     assert len(stimuli) == 15 * 2 * 15
-    assert len(np.unique(stimuli['family'])) == 15
-    assert len(np.unique(stimuli['type'])) == 2
+    assert len(np.unique(stimuli['texture_family'])) == 15
+    assert len(np.unique(stimuli['texture_type'])) == 2
     assert len(np.unique(stimuli['sample'])) == 15
     return stimuli
 
 
-def load_responses(data_path):
+def load_responses(response_file, stimuli_directory):
     # from the readme.m: data is in the form:
     # (texFamily) x (texType) x (sample) x (rep) x (timeBin) x (cellNum)
     # (15)        x (2)       x (15)     x (20)  x (300)     x (102+)
     # in python, ordering is inverted:
     # (cellNum) x (timeBin) x (rep) x (sample) x (texType) x (texFamily)
     # (102+)    x (300)     x (20)  x (15)     x (2)       x (15)
-    responses = h5py.File(os.path.join(data_path, 'data', 'FreemanZiemba2013_V1V2data.mat'), 'r')
+    responses = h5py.File(response_file, 'r')
     v1, v2 = responses['v1'], responses['v2']
     assert v1.shape[1:] == v2.shape[1:]  # same except cells
     responses = np.concatenate([v1, v2])
 
     assembly = xr.DataArray(responses,
                             coords={
-                                'neuroid_id': list(range(responses.shape[0])),
-                                'region': ('neuroid_id', ['V1'] * v1.shape[0] + ['V2'] * v2.shape[0]),
-                                'time_bin': list(range(responses.shape[1])),
+                                'neuroid_id': ("neuroid", list(range(1, responses.shape[0] + 1))),
+                                'region': ('neuroid', ['V1'] * v1.shape[0] + ['V2'] * v2.shape[0]),
+                                'time_bin_start': ("time_bin", list(range(responses.shape[1]))),  # each bin is 1 ms
+                                'time_bin_end': ("time_bin", list(range(1, responses.shape[1] + 1))),
                                 'repetition': list(range(responses.shape[2])),
-                                'sample': list(range(responses.shape[3])),
-                                'texture_type': list(range(responses.shape[4])),
-                                'texture_family': list(range(responses.shape[5]))
+                                'sample': list(range(1, responses.shape[3] + 1)),
+                                'texture_type': ["noise", "texture"],
+                                'texture_family': textureNumOrder
                             },
-                            dims=['neuroid_id', 'time_bin', 'repetition', 'sample', 'texture_type', 'texture_family'])
-    assembly = assembly.stack(neuroid=['neuroid_id'])
+                            dims=['neuroid', 'time_bin', 'repetition', 'sample', 'texture_type', 'texture_family'])
+
+    assembly = assembly.stack(presentation=['texture_type', 'texture_family', 'sample', 'repetition'])
+
+    image_fields = zip(*[assembly[k].values for k in ['texture_type', 'texture_family', 'sample']])
+    image_names = [image_name_from_fields(im[0], "320x320", im[1], im[2]) for im in image_fields]
+    assembly["image_file_name"] = ("presentation", image_names)
+
+    kfs = {}
+    sha1s = []
+    for image_name in image_names:
+        if image_name in kfs:
+            im_kf = kfs[image_name]
+        else:
+            im_kf = kf(os.path.join(stimuli_directory, image_name))
+            kfs[image_name] = im_kf
+        sha1s.append(im_kf.sha1)
+    assembly["image_id"] = ("presentation", sha1s)
+
     return assembly
 
 
@@ -75,42 +92,36 @@ def walk_coords(assembly):  # from brain-score/brainscore/assemblies.py
     return coords
 
 
+def image_name_from_fields(texture_type, resolution, texture_family, sample):
+    mapping = {"noise": "noise", "texture": "tex"}
+    return f"{mapping[texture_type]}-{resolution}-im{int(texture_family)}-smp{int(sample)}.png"
+
+
+def fields_from_image_name(image_name):
+    # sample filename: noise-320x320-im13-smp8
+    integer_fields = ['family', 'sample']
+    mapping = {"noise": "noise", "tex": "texture"}
+    pattern = "^(?P<texture_type>[^-]+)-(?P<resolution>[^-]+)-im(?P<texture_family>[0-9]*)-smp(?P<sample>[0-9]+)\.png$"
+    match = re.match(pattern, image_name)
+    assert match
+    fields = match.groupdict()
+    fields = {field: value if field not in integer_fields else int(value) for field, value in fields.items()}
+    fields = {field: value if field != "texture_type" else mapping[value] for field, value in fields.items()}
+    return fields
+
+
 def main():
     data_path = os.path.join(os.path.dirname(__file__), 'FreemanZiemba2013')
+    stimuli_directory = os.path.join(data_path, 'stim')
+    response_file = os.path.join(data_path, 'data', 'FreemanZiemba2013_V1V2data.mat')
 
-    stimuli = load_stimuli(data_path)
-    responses = load_responses(data_path)
+    stimuli = load_stimuli(stimuli_directory)
+    assembly = load_responses(response_file, stimuli_directory)
 
-    # fix response stimulus annotations
-    def build_mapping(response_key, stimuli_key):
-        assert len(np.unique(responses[response_key])) == len(np.unique(stimuli[stimuli_key]))
-        return {response_family: stimuli_family for response_family, stimuli_family in
-                zip(np.unique(responses[response_key]), np.unique(stimuli[stimuli_key]))}
-
-    mapping_keys = {'texture_family': 'family', 'texture_type': 'type', 'sample': 'sample'}
-    assert np.prod([shape for shape, include in
-                    zip(responses.shape, [dim in mapping_keys for dim in responses.dims]) if include]) \
-           == len(stimuli), "number of stimuli do not match"
-    mappings = {response_key: build_mapping(response_key, stimuli_key)
-                for response_key, stimuli_key in mapping_keys.items()}
-    coords = {coord: (dims, values if coord not in mappings else [mappings[coord][value] for value in values])
-              for coord, dims, values in walk_coords(responses)}
-    assembly = xr.DataArray(responses.values, coords=coords, dims=responses.dims)
-    assembly = assembly.stack(presentation=['texture_type', 'texture_family', 'sample', 'repetition'])
-    presentations = pd.DataFrame({key: values for key, dims, values in walk_coords(assembly['presentation'])})
-
-    # attach filepaths
-    filepaths = []
-    for _, row in presentations.iterrows():
-        row_filter = [stimuli[stimuli_key] == row[response_key] for response_key, stimuli_key in mapping_keys.items()]
-        row_filter = np.array(row_filter).all(axis=0)
-        stimuli_row = stimuli[row_filter]
-        filepath = stimuli_row['filepath']
-        assert len(filepath) == 1, "expected exactly one match"
-        filepaths.append(filepath.values[0])
-    assembly['stimuli_path'] = 'presentation', filepaths
-
-    return assembly
+    print(assembly)
+    nonzero = np.count_nonzero(assembly)
+    print(nonzero)
+    assert nonzero > 0
 
 
 if __name__ == '__main__':
