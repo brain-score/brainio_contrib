275a208af2b4c1bc2196fe326c962b44749b12fc
Jonathan Prescott-Roy <jjpr@mit.edu>
2019-05-01 11:40:17 -0400

    Basic tests for packaging methods.

---
+++ brainio_contrib/packaging.py
@@ -0,0 +1,15 @@
+
+
+
+
+def package_stimulus_set(proto_stimulus_set):
+    """
+
+    :param proto_stimulus_set:
+    :return:
+    """
+    pass
+
+
+
+
---
+++ tests/test_packaging.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def test_package_stimulus_set():
+    test_proto = pd.read_csv("images/test_images.csv")
+    
