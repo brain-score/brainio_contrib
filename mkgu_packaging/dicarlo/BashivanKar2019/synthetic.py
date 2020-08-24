761a4e93adddb3555ee90dfc53bfbcf4425af7ef
Jonathan Prescott-Roy <jjpr@mit.edu>
2020-08-24 00:28:22 -0400

    runs to packaging synth assemblies, chokes on bool

--- mkgu_packaging/dicarlo/BashivanKar2019/synthetic.py
+++ mkgu_packaging/dicarlo/BashivanKar2019/synthetic.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 import logging
-import json
 
 import numpy as np
 import xarray as xr
@@ -47,14 +46,14 @@ def np_to_xr(monkey, setting, session_neural, stimuli, session_target_inds, stag
         "repetition_index": ("repetition", range(session_neural.shape[0])),
         "image_id": ("image", stimuli["image_id"]),
         "neuroid_id": ("neuroid", neuroid_id),
-        "animal": ("neuroid", monkey._v_name),
-        "setting": ("neuroid", setting._v_name),
-        "session": ("neuroid", session_neural._v_name),
+        "animal": ("neuroid", [monkey._v_name]*session_neural.shape[2]),
+        "setting": ("neuroid", [setting._v_name]*session_neural.shape[2]),
+        "session": ("neuroid", [session_neural._v_name]*session_neural.shape[2]),
         "neuroid_index": ("neuroid", range(session_neural.shape[2])),
         "is_target": ("neuroid", is_target)
     }
-    assert stimuli["image_index"] == range(session_neural.shape[1])
-    proto = xr.DataArray(session_neural, coords=coords, dims=dims)
+    assert all(stimuli["image_index"] == range(session_neural.shape[1]))
+    proto = xr.DataArray(session_neural.read(), coords=coords, dims=dims)
     proto = proto.stack(presentation=('image', 'repetition')).reset_index('presentation')
     proto = proto.drop('image')
     proto = proto.expand_dims('time_bin')
@@ -88,41 +87,6 @@ def collect_responses_nat(h5, stimuli):
     return responses_nat_d
 
 
-def collect_stimuli_synth(h5, data_dir):
-    protos_stimuli = []
-    for monkey in h5.root.images.synthetic:
-        for setting in monkey:
-            for session in setting:
-                identifier = f"{monkey._v_name[-1]}_{setting._v_name}_{session._v_name}"
-                img_temp_path = data_dir / "images_temp" / "synthetic" / identifier
-                img_temp_path.mkdir(parents=True, exist_ok=True)
-                session_proto = np_to_png(session, img_temp_path)
-                session_proto["animal"] = monkey._v_name
-                session_proto["setting"] = setting._v_name
-                session_proto["session"] = session._v_name
-                protos_stimuli.append(session_proto)
-    proto_stimuli_all = pd.concat(protos_stimuli, axis=0)
-    assert len(np.unique(proto_stimuli_all['image_id'])) == len(proto_stimuli_all)
-    stimuli = StimulusSet(proto_stimuli_all)
-    stimuli.image_paths = {row.image_id: row.image_current_local_file_path for row in stimuli.itertuples()}
-    return stimuli
-
-
-def collect_responses_synth(h5, data_dir, stimuli):
-    responses_synth_d = {}
-    for monkey in h5.root.neural.synthetic:
-        for setting in monkey:
-            for session in setting:
-                target_inds_session = h5.root.target_inds[monkey._v_name][setting._v_name][session._v_name]
-                proto = np_to_xr(monkey, setting, session, stimuli, target_inds_session, "synth")
-                responses_synth_d[proto.name] = proto
-    return responses_synth_d
-
-
-
-
-
-
 def collect_synth(h5, data_dir):
     protos_stimuli = []
     responses_synth_d = {}
@@ -153,7 +117,6 @@ def collect_synth(h5, data_dir):
 
 
 def main():
-    # data_dir = Path(__file__).parents[6] / 'data2' / 'active' / 'users' / 'jjpr'
     data_dir = Path("/Users/jjpr/dev/brainio_contrib/mkgu_packaging/dicarlo/BashivanKar2019")
     assert os.path.isdir(data_dir)
     h5_path = data_dir / "from_pouya" / "npc_v4_data.h5"
@@ -164,19 +127,8 @@ def main():
 
     responses_nat_d = collect_responses_nat(h5, stimuli_nat)
 
-    stimuli_synth = collect_stimuli_synth(h5, data_dir)
-    stimuli_synth.identifier = "dicarlo.BashivanKar2019.synthetic"
-
-    responses_synth_d = collect_responses_synth(h5, data_dir, stimuli_synth)
-
-
-
-
-
     stimuli_synth, responses_synth_d = collect_synth(h5, data_dir)
-
-
-
+    stimuli_synth.identifier = "dicarlo.BashivanKar2019.synthetic"
 
     _logger.debug('Packaging naturalistic stimuli')
     package_stimulus_set(stimuli_nat, stimulus_set_identifier=stimuli_nat.identifier, bucket_name='brainio.dicarlo')
